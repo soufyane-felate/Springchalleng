7a1601a7d5cfa9c3463a23029eff821764ef69f2
soufyane-felate <soufyanfellat03@gmail.com>
2025-05-09 10:05:11 +0100

    challenge

--- src/main/java/com/challenge/challenge/repositories/ItemRepository.java
+++ src/main/java/com/challenge/challenge/repositories/ItemRepository.java
@@ -1,18 +1,18 @@
 package com.challenge.challenge.repositories;
 
 import com.challenge.challenge.entities.Product;
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Product, Long> {
 //challenge 2
-    @Query("SELECT i FROM Item i WHERE i.category = :category")
+    @Query("SELECT i FROM Product i WHERE i.category = :category")
     List<Product> findByCategory(@Param("category") String category);
 
-    @Query("SELECT i FROM Item i WHERE i.price < :maxPrice")
+    @Query("SELECT i FROM Product i WHERE i.price < :maxPrice")
     List<Product> findByLessThen(@Param("maxPrice") double maxPrice);
 
     @Query(value = "SELECT * FROM item WHERE LOWER(name) = LOWER(:name)", nativeQuery = true)
